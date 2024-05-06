e580acf5405f54dfee9f1b79125e77d08de25157
MrDiceRack <160239141+MrDiceRack@users.noreply.github.com>
2024-05-06 11:43:23 +0800

    Update create_csv.py
    Replaced the member[5] calls to member.find('bndbox') for more versatility. The member[5] calls were producing an error to Roboflow generated Pascal VOC XML files.

--- util_scripts/create_csv.py
+++ util_scripts/create_csv.py
@@ -12,19 +12,21 @@ def xml_to_csv(path):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
+            # print(member.find('bndbox')[0].text)
             value = (root.find('filename').text,
                      int(root.find('size')[0].text),
                      int(root.find('size')[1].text),
                      member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
+                     int(member.find('bndbox')[0].text),
+                     int(member.find('bndbox')[1].text),
+                     int(member.find('bndbox')[2].text),
+                     int(member.find('bndbox')[3].text)
                      )
             xml_list.append(value)
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     return xml_df
+
 
 def main():
     for folder in ['train','validation']:
@@ -32,5 +34,6 @@ def main():
         xml_df = xml_to_csv(image_path)
         xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
         print('Successfully converted xml to csv.')
+
 
 main()
